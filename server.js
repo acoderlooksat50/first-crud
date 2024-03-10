1da6393c64f7cd86e8ab57e3e4e20da19ccd483c
acoderlooksat50 <acoderlooksat50@gmail.com>
2024-03-10 16:01:09 -0400

    add ejs and mongodb connection

--- server.js
+++ server.js
@@ -4,21 +4,39 @@ const app = express()
 const bodyParser = require('body-parser') //used to parse input (post) data
 
 const MongoClient = require('mongodb').MongoClient //used to access the MongoDB database
+const connectionString = 'mongodb+srv://swopimp:kuTCAYDoE3adTDoR@cluster0.1rrxk54.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+MongoClient.connect(connectionString)
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('star-wars-quotes')
+        const quotesCollection = db.collection('quotes')
+
+        app.set('view engine', 'ejs')
+        app.use(bodyParser.urlencoded({extended: true})) //command to use body-parser. Must be before handlers
+
+        app.listen(8000, ()=> console.log('listening on port 8000')) //sets server to listen on port, consoles that the port is active
+
+        app.get('/', (req,res) => {
+            quotesCollection.find().toArray()  //find all quotes and place into an array
+                .then(results => {
+                    console.log(results)
+                })
+                .catch(error => console.error (error))
+
+            res.sendFile(__dirname + '/index.html')
+            })  //when browser contacts the server it is looking to GET information.  This gets the index.htm file from the current directory.
+
+        app.post('/quotes', (req,res) => {
+            quotesCollection.insertOne(req.body)
+                .then(result => {
+                    console.log(result)
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+            })  //posts something to the database.
+    })
+    .catch(error => console.error(error))
 
 
-app.use(bodyParser.urlencoded({extended: true})) //command to use body-parser. Must be before handlers
 
-app.listen(8000, ()=> console.log('listening on port 8000')) //sets server to listen on port, consoles that the port is active
-
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html')
-})  //when browser contacts the server it is looking to GET information.  This gets the index.htm file from the current directory.
-
-app.post('/quotes', (req,res) => {
-    console.log(req.body)
-})  //posts something to the server.
-
-MongoClient.connect('mongodb+srv://swopimp:<password>@cluster0.nptjtym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', (err,client) => {
-    if(err) return console.error(err)
-    console.log('Connected to Database')
-})
